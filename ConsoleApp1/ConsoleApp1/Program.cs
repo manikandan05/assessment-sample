fc1b96f4cccbc8fc83b099213147315be8f34a09
manikandan <mani_kandan05@yahoo.com>
2020-12-06 13:24:20 +0530

    Sample added.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //first program
+
+            string data = Console.ReadLine();
+            string newData = "";
+            if (data.Length > 0)
+            {
+                for (var i = 0; i < data.Length; i++)
+                {
+                    if (!(data[i] == 'a' || data[i] == 'e' || data[i] == 'i' || data[i] == 'o' || data[i] == 'u'))
+                    {
+                        newData += data[i];
+                    }
+                }
+            }
+            Console.WriteLine(newData);
+
+
+            //second program
+
+            string newArrayData = Console.ReadLine();
+
+            string[] newList = newArrayData.Split(",");
+            int count = newList.Length;
+            int higherbound = Int16.MinValue;
+            int lowerbound = Int16.MaxValue;
+            if (count > 0)
+            {
+                for (var j = 0; j < count; j++)
+                {
+                    if (Int16.Parse(newList[j]) > higherbound)
+                    {
+                        higherbound = Int16.Parse(newList[j]);
+                    }
+                }
+                for (var j = 0; j < count; j++)
+                {
+                    if (Int16.Parse(newList[j]) < lowerbound)
+                    {
+                        lowerbound = Int16.Parse(newList[j]);
+                    }
+                }
+            }
+            Console.WriteLine("UpperBound" + higherbound);
+            Console.WriteLine("LowerBound" + lowerbound);
+
+
+            //Third program
+            string[] arrList = data.Split("");
+            List<string> compareList = new List<string>();
+            List<string> shortList = new List<string>();
+            if (arrList.Length > 0)
+            {
+                for (var k = 0; k < arrList.Length; k++)
+                {
+                    if (compareList.Count == 0 || compareList[0].IndexOf(arrList[k]) == -1)
+                    {
+                        compareList.Add(arrList[k]);
+                    }
+                    else
+                    {
+                        compareList.Add(arrList[k]);
+                        shortList = compareList;
+                        compareList = new List<string>();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Data.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+
+    public class PhoneNumber
+    {
+        public int Number { get; set; }
+    }
+
+    public class Data
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<PhoneNumber> PhoneNumber { get; set; }
+
+        public static List<Data> ResultData()
+        {
+            List<Data> result = new List<Data>();
+            List<PhoneNumber> number = new List<PhoneNumber>();
+            number.Add(new PhoneNumber { Number = 123567 });
+
+            List<PhoneNumber> number1 = new List<PhoneNumber>();
+            number1.Add(new PhoneNumber { Number = 123567 });
+            number1.Add(new PhoneNumber { Number = 09876543 });
+
+            result.Add(new Data { ID = 1, Name = "ABC", PhoneNumber = number });
+            result.Add(new Data { ID = 2, Name = "xyz", PhoneNumber = number1 });
+            result.Add(new Data { ID = 3, Name = "123", PhoneNumber = number });
+            return result;
+        }
+    }
+}
